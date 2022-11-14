12368ba8c25f877f31f6cc25ccdea3d6e02a4214
unknown <muhamadrafli419@gmail.com>
2022-11-14 17:57:50 +0700

    app

--- app/Controllers/c_main.php
+++ app/Controllers/c_main.php
@@ -2,11 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\m_mapel;
 class c_main extends BaseController
 {
+    protected $mapelModel;
+    public function __construct()
+    {
+        $this->mapelModel = new m_mapel();
+    }
     public function display()
     {
         $data['content_view'] = "v_index";
+        echo view('v_template', $data);
+    }
+    public function kelas()
+    {
+        $data['content_view'] = "v_kelas";
+        $data['mapel'] = $this->mapelModel->get_mapel();
         echo view('v_template', $data);
     }
 }
