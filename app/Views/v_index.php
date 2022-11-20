fc74b4ed4387c6e84cd3af00ec2696c666377cb1
Thorrr <42139609+thoriq1520@users.noreply.github.com>
2022-11-20 21:22:05 +0700

    Finalv2

--- app/Views/v_index.php
+++ app/Views/v_index.php
@@ -93,27 +93,32 @@
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                 <h6 class="section-title bg-white text-center text-primary px-3">Categories</h6>
                 <h1 class="mb-5">Courses Categories</h1>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#tambahMapel">
+                <i class="fa fa-plus">   Tambah Mapel</i>
+            </button>
             </div>
+            </br>
+            </br>
             <div class="row g-3">
 
-<div>
+                <div>
 
-    <div class="row g-3 justify-content-center">
+                    <div class="row g-3 justify-content-center">
                         <!-- Kursus -->
                         <?php
-                    foreach ($mapel->getResultArray() as $m) :
-                    ?>
-                        <div class="col-lg-3 col-md-12 wow zoomIn" data-wow-delay="0.3s">
-                            <a class="position-relative d-block overflow-hidden" href="<?= base_url('semua_materi/'. $m['nama']); ?>">
-                                <img class="img-fluid" src="img/cat-2.jpg" alt="">
-                                <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
-                                    <h5 class="m-0"><?php echo $m['nama'] ?></h5>
-                                </div>
-                            </a>
-                        </div>
+                        foreach ($mapel->getResultArray() as $m) :
+                        ?>
+                            <div class="col-lg-3 col-md-12 wow zoomIn" data-wow-delay="0.3s">
+                                <a class="position-relative d-block overflow-hidden" href="<?= base_url('semua_materi/' . $m['nama']); ?>">
+                                    <img class="img-fluid" src="img/cat-2.jpg" alt="">
+                                    <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
+                                        <h5 class="m-0"><?php echo $m['nama'] ?></h5>
+                                    </div>
+                                </a>
+                            </div>
                         <?php
-                    endforeach;
-                    ?>    
+                        endforeach;
+                        ?>
                         <!-- Kursus -->
                     </div>
                 </div>
@@ -157,3 +162,49 @@
     ?>
 
     <!-- Courses End -->
+
+    <!-- Button trigger modal -->
+
+
+<div class="modal fade" id="tambahMapel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Mapel</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="/post_materi" enctype="multipart/form-data">
+                    <fieldset>
+                        <div class="row mb-3">
+                            <div class="col-sm-10">
+                                <input type="hidden" name="nama_mapel"  value="" id="disabledTextInput" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                    </fieldset>
+                    <div class="row mb-3">
+                        <label for="materi" class="col-sm-2 col-form-label col-form-label-sm">Mapel</label>
+                        <div class="col-sm-10">
+                            <input type="text" id="materi" name="judul_materi" value="" class="form-control form-control-sm" placeholder="Nama Mapel">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-10">
+                        <input type="hidden" name="kelas"  value="" id="disabledTextInput" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
